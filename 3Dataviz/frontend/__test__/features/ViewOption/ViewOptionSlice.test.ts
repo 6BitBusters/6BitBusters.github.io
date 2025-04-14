fd5e92d8578efb46bc8d1cdf4ff1b6d9a9ae356c
Andrea Soranzo <andreajsm03@gmail.com>
2025-04-14 17:11:34 +0200

    change: aggiunto viewoptionslice

---
+++ 3Dataviz/frontend/__test__/features/ViewOption/ViewOptionSlice.test.ts
@@ -0,0 +1,37 @@
+import { describe, expect, it } from "vitest";
+import reducer, {
+  selectorViewOptionState,
+  toggleAveragePlane,
+} from "../../../src/features/ViewOption/ViewOptionSlice";
+import { ViewOptionState } from "../../../src/features/ViewOption/types/ViewOptionState";
+
+describe("ViewOptionSlice", () => {
+  it("Rendi il piano visibile", () => {
+    const initialState: ViewOptionState = {
+      isPlaneActive: false,
+    };
+    const expectedState: ViewOptionState = {
+      isPlaneActive: true,
+    };
+    expect(reducer(initialState, toggleAveragePlane(true))).toEqual(
+      expectedState,
+    );
+  });
+  it("Rendi il piano invisibile", () => {
+    const initialState: ViewOptionState = {
+      isPlaneActive: true,
+    };
+    const expectedState: ViewOptionState = {
+      isPlaneActive: false,
+    };
+    expect(reducer(initialState, toggleAveragePlane(false))).toEqual(
+      expectedState,
+    );
+  });
+  it("Prendere lo stato", () => {
+    const state: ViewOptionState = {
+      isPlaneActive: true,
+    };
+    expect(selectorViewOptionState(state)).toEqual(state);
+  });
+});
--- 3Dataviz/frontend/src/app/RootReducer.ts
+++ 3Dataviz/frontend/src/app/RootReducer.ts
@@ -2,11 +2,13 @@ import { combineReducers } from "@reduxjs/toolkit";
 import dataSlice from "../features/Data/DataSlice";
 import AppStatusSlice from "../features/AppStatus/AppStatusSlice";
 import DataSourceSlice from "../features/DataSource/DataSourceSlice";
+import ViewOptionSlice from "../features/ViewOption/ViewOptionSlice";
 
 const rootReducer = combineReducers({
   data: dataSlice,
   appState: AppStatusSlice,
   dataSource: DataSourceSlice,
+  viewOption: ViewOptionSlice,
 });
 
 export default rootReducer;
