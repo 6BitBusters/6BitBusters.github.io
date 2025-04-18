6c4dae3da6fe92be2d8eb730ab3ec026e257240c
Soranzo Andrea <56091337+AndreaSoranzo@users.noreply.github.com>
2025-04-18 22:14:27 +0200

    Frontend - 281 (#302)
    * change: riscritto il AppStatusSlice e i test
    
    * change: start action
    
    * change: start action 2
    
    * change: correzioni
    
    * change: riscrittura degli errori
    
    * change: aggiunto viewoptionslice
    
    * change: semplificato il selector
    
    * change: RootState
    
    * change: tests

--- 3Dataviz/frontend/src/app/RootReducer.ts
+++ 3Dataviz/frontend/src/app/RootReducer.ts
@@ -2,6 +2,7 @@ import { combineReducers } from "@reduxjs/toolkit";
 import DataSlice from "../features/Data/DataSlice";
 import AppSlice from "../features/AppStatus/AppSlice";
 import DataSourceSlice from "../features/DataSource/DataSourceSlice";
+import ViewOptionSlice from "../features/ViewOption/ViewOptionSlice";
 import RaycastHitSlice from "../features/Raycast/RaycastHitSlice";
 
 const rootReducer = combineReducers({
@@ -9,6 +10,7 @@ const rootReducer = combineReducers({
   appState: AppSlice,
   dataSource: DataSourceSlice,
   raycast: RaycastHitSlice,
+  viewOption: ViewOptionSlice,
 });
 
 export default rootReducer;
