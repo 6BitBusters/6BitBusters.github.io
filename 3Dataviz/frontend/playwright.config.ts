d0e3c6b1c83cf48afc2d08d75aa9387c998ad7b2
Andrea Soranzo <andreajsm03@gmail.com>
2025-04-22 17:50:24 +0200

    change: aggiornato il e2e config

--- 3Dataviz/frontend/playwright.config.ts
+++ 3Dataviz/frontend/playwright.config.ts
@@ -24,6 +24,12 @@ export default defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+  webServer: {
+    command: 'npm start',
+    url: "http://localhost:5173",
+    timeout: 120000,
+    reuseExistingServer: !process.env.CI,
+  },
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: "http://localhost:5173",
