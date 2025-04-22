f2668d2f806fec1a82ef09af4a3d1ac343d01599
Andrea Soranzo <andreajsm03@gmail.com>
2025-04-22 17:32:11 +0200

    change: test E2E

--- 3Dataviz/frontend/cypress/e2e/CustomCanvas.cy.ts
+++
@@ -1,9 +0,0 @@
-describe("Canvas", () => {
-  beforeEach(() => {
-    cy.visit("/"); // Assicurati che la tua app Three.js sia in esecuzione su questa URL
-  });
-
-  it("Carimamento del canvas", () => {
-    cy.get("canvas").should("be.visible");
-  });
-});
