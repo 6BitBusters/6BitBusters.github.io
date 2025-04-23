926065216809e310695a318c335e185b345de730
FilippoDiviesti <filippo.diviesti@studenti.unipd.it>
2025-04-23 13:14:20 +0200

    add: test HomePage

--- 3Dataviz/frontend/__test__/pages/HomePage/HomePage.test.tsx
+++ 3Dataviz/frontend/__test__/pages/HomePage/HomePage.test.tsx
@@ -1,9 +1,70 @@
-import { render } from "@testing-library/react";
 import { beforeEach, describe, expect, it } from "vitest";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import HomePage from "../../../src/pages/HomePage/HomePage";
+import { thunk } from "redux-thunk";
+import configureMockStore from "redux-mock-store";
+import { AppDispatch, RootState } from "../../../src/app/Store";
+import fetchMock from "fetch-mock";
+import { Provider } from "react-redux";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import { CreateMockRootState } from "../../utils/StateMockCreator";
+import { DataSourceState } from "../../../src/features/DataSource/types/DataSourceState";
+import { DatasetInfo } from "../../../src/features/DataSource/types/DatasetInfo";
+import "@testing-library/jest-dom";
+import React from "react";
+import { findByText } from "@testing-library/react";
+
+const middlewares = [thunk];
+const mockStore = configureMockStore<RootState, AppDispatch>(middlewares);
+const ROUTER_test = createBrowserRouter([
+  {
+    path: "/",
+    Component: HomePage,
+  },
+]);
 
 describe("HomePage", () => {
-  it("Non dovrebbero esserci errori nell'AppStatus", () => {});
-  it("dovrebbe caricare correttamente la HomePage", () => {});
-  it("dovrebbe caricare correttamente ApiSelector", () => {});
-  it("dovrebbe caricare correttamente Footer", () => {});
+  beforeEach(() => {
+    fetchMock.unmockGlobal();
+    fetchMock.removeRoutes();
+  });
+
+  it("Non dovrebbero esserci errori nell'AppStatus", () => {
+    const store = mockStore(CreateMockRootState());
+    render(
+      <Provider store={store}>
+        <RouterProvider router={ROUTER_test} />
+      </Provider>,
+    );
+    const state = store.getState();
+    expect(state.appState.error).toEqual(null);
+  });
+
+  it("dovrebbe caricare correttamente la HomePage", () => {
+    render(
+      <Provider store={mockStore(CreateMockRootState())}>
+        <RouterProvider router={ROUTER_test} />
+      </Provider>,
+    );
+
+    expect(screen.getByText("3DataViz")).toBeInTheDocument();
+  });
+
+  it("dovrebbe caricare correttamente ApiSelector", () => {
+    render(
+      <Provider store={mockStore(CreateMockRootState())}>
+        <RouterProvider router={ROUTER_test} />
+      </Provider>,
+    );
+    expect(screen.getByText("Scegli una API...")).toBeInTheDocument();
+  });
+
+  it("dovrebbe caricare correttamente Footer", () => {
+    render(
+      <Provider store={mockStore(CreateMockRootState())}>
+        <RouterProvider router={ROUTER_test} />
+      </Provider>,
+    );
+    expect(screen.getByText(/Copyright/)).toBeInTheDocument();
+  });
 });
